9f7a052b3f0d32bf74cefb1b723dfb8a7e877518
Vasco Ferreira <vasco.santos.ferreira@cgd.pt>
2018-10-15 15:36:22 +0100

    making splash show only when app is really on background, stop showing on native alerts

--- src/ios/BackgroundSplashPlugin.m
+++ src/ios/BackgroundSplashPlugin.m
@@ -6,11 +6,12 @@ static UIImageView *imageView;
 
 - (void)pluginInitialize
 {
- // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive:)
- //                                              name:UIApplicationDidBecomeActiveNotification object:nil];
 
- // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:)
-  //                                             name:UIApplicationWillResignActiveNotification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive:)
+                                               name:UIApplicationDidBecomeActiveNotification object:nil];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground:)
+                                               name:UIApplicationDidEnterBackgroundNotification object:nil];
 }
 
 
@@ -18,77 +19,53 @@ static UIImageView *imageView;
 -(void) handleBackgroundViewMode:(BOOL) active {
     
     
-    // a app entrou em foreground , retirar a bacground controller
+    
+    // app entered foreground , remove background controller
     if ( active) {
         
-        if ( self.backgroundModeView != nil) {
-            
-            [self.backgroundModeView removeFromSuperview];
-            self.backgroundModeView = nil;
-            
+        if (imageView == NULL) {
+            self.viewController.view.window.hidden = NO;
+        } else {
+            [imageView removeFromSuperview];
         }
         
     }
     else {
-        // passou pra background , por o background a mostrar
-        
-        self.backgroundModeView = [[UIView alloc ] initWithFrame:self.navigationController.topViewController.view.frame];
-        self.backgroundModeView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-        
-        
-        UIImageView *imgView = nil;
-        UIImage *backgroundImage = nil;
-        
-        if ( ISIPHONE ) {
-            
-            CGSize dimensaoEcran = [[UIScreen mainScreen] bounds].size;
-            
-            backgroundImage = [UIImage imageNamed:@"Default"];
-            imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, dimensaoEcran.width, dimensaoEcran.height)];
-            
+
+        CDVViewController *vc = (CDVViewController*)self.viewController;
+        NSString* imageName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UILaunchImageFile"];
+
+        if (imageName) {
+            imageName = [imageName stringByDeletingPathExtension];
+        } else {
+            imageName = @"Default";
         }
-        else {
-            
-            UIInterfaceOrientation uiOrientation = [[UIApplication sharedApplication] statusBarOrientation];
-            switch ( uiOrientation ) {
-                    
-                case UIInterfaceOrientationPortrait:
-                case UIInterfaceOrientationPortraitUpsideDown:
-                    
-                    imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 768, 1024)];
-                    backgroundImage = [UIImage imageNamed:@"Default-Portrait~ipad"];
-                    break;
-                    
-                case UIInterfaceOrientationLandscapeRight:
-                case UIInterfaceOrientationLandscapeLeft:
-                    
-                    imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1024, 768)];
-                    backgroundImage = [UIImage imageNamed:@"Default-Landscape~ipad.png"];
-                    break;
-                    
-                default:
-                    NSLog(@"handleBackgroundViewMode uiinterfaceOrientation not handled: %ld", (long)uiOrientation);
-                    
-            }
-        }
-        imgView.image = backgroundImage;
-        
-        [self.backgroundModeView addSubview:imgView];
-        
-        [self.navigationController.topViewController.view addSubview:self.backgroundModeView];
-        
+
+
+        UIImage *splash = [UIImage imageNamed:imageName];
+        if (splash == NULL) {
+            imageView = NULL;
+            self.viewController.view.window.hidden = YES;
+        } else {
+            imageView = [[UIImageView alloc]initWithFrame:[self.viewController.view bounds]];
+            [imageView setImage:splash];
+    
+        #ifdef __CORDOVA_4_0_0
+            [[UIApplication sharedApplication].keyWindow addSubview:imageView];
+        #else
+            [self.viewController.view addSubview:imageView];
+        #endif
+
+
         
     }
-    
-    
+   } 
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    
+
     [self handleBackgroundViewMode:NO];
     
 }

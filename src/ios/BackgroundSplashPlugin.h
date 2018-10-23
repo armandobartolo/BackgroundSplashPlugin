928f0ddfb6d2bf6e209b8da3b260ed9b20675868
Vasco Ferreira <vasco.santos.ferreira@cgd.pt>
2018-10-23 17:23:04 +0100

    resizing by phone

--- src/ios/BackgroundSplashPlugin.h
+++ src/ios/BackgroundSplashPlugin.h
@@ -3,7 +3,15 @@
 
 typedef struct {
   
-  
+  BOOL iPhone;
+  BOOL iPad;
+  BOOL iPhone4;
+  BOOL iPhone5;
+  BOOL iPhone6;
+  BOOL iPhone6Plus;
+  BOOL iPhoneX;
+  BOOL iPhoneXrAndXsMax;
+  BOOL retina;
 } CDV_iOSDevice;
 
 @interface BackgroundSplashPlugin : CDVPlugin
--- src/ios/BackgroundSplashPlugin.m
+++ src/ios/BackgroundSplashPlugin.m
@@ -18,9 +18,6 @@ static UIImageView *imageView;
 
 -(void) handleBackgroundViewMode:(BOOL) active {
     
-    
-    
-    // app entered foreground , remove background controller
     if ( active) {
         
         if (imageView == NULL) {
@@ -33,14 +30,9 @@ static UIImageView *imageView;
     else {
 
         CDVViewController *vc = (CDVViewController*)self.viewController;
-        NSString* imageName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UILaunchImageFile"];
-
-        if (imageName) {
-            imageName = [imageName stringByDeletingPathExtension];
-        } else {
-            imageName = @"Default";
-        }
-
+     
+NSString *imageName = [self getImageName: [self getCurrentDevice]];
+  
 
         UIImage *splash = [UIImage imageNamed:imageName];
         if (splash == NULL) {
@@ -56,8 +48,6 @@ static UIImageView *imageView;
             [self.viewController.view addSubview:imageView];
         #endif
 
-
-        
     }
    } 
 }
@@ -75,6 +65,64 @@ static UIImageView *imageView;
    
     [self handleBackgroundViewMode:YES];
     
+}
+
+// code borrowed from cordova-plugin-splashscreen and added the new iPhone X's sizes has on -> https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
+
+- (CDV_iOSDevice) getCurrentDevice
+{
+  CDV_iOSDevice device;
+  
+  UIScreen* mainScreen = [UIScreen mainScreen];
+  CGFloat mainScreenHeight = mainScreen.bounds.size.height;
+  CGFloat mainScreenWidth = mainScreen.bounds.size.width;
+  
+  int limit = MAX(mainScreenHeight,mainScreenWidth);
+  
+  device.iPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+  device.iPhone = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone);
+  device.retina = ([mainScreen scale] == 2.0);
+  device.iPhone4 = (device.iPhone && limit == 480.0);
+  device.iPhone5 = (device.iPhone && limit == 568.0);
+  device.iPhone6 = (device.iPhone && limit == 667.0);
+  device.iPhone6Plus = (device.iPhone && limit == 736.0);
+  device.iPhoneX = (device.iPhone && limit == 812.0);
+  device.iPhoneXrAndXsMax = (device.iPhone && limit == 896.0);
+  
+  return device;
+}
+
+- (NSString*)getImageName: (CDV_iOSDevice)device
+{
+  NSString* imageName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UILaunchImageFile"];
+  
+  if (imageName) {
+    imageName = [imageName stringByDeletingPathExtension];
+  } else {
+    imageName = @"Default";
+  }
+//There is some deelay while geting the original resources for iPhone X and after, so we use iPhone6 resources
+  if ([imageName isEqualToString:@"LaunchImage"])
+  {
+    if(device.iPhone4 || device.iPhone5 || device.iPad) {
+      imageName = [imageName stringByAppendingString:@"-700"];
+    } else if(device.iPhone6 || device.iPhoneX || device.iPhoneXrAndXsMax) {
+      imageName = [imageName stringByAppendingString:@"-800"];
+    } else if(device.iPhone6Plus) {
+       imageName = [imageName stringByAppendingString:@"-800"];    
+    } 
+  }
+  
+  
+  if (device.iPhone5) { 
+    imageName = [imageName stringByAppendingString:@"-568h"];
+  } else if (device.iPhone6 || device.iPhoneX || device.iPhoneXrAndXsMax) { 
+    imageName = [imageName stringByAppendingString:@"-667h"];
+  } else if (device.iPhone6Plus) { 
+    imageName = [imageName stringByAppendingString:@"-736h"];    
+  } 
+  
+  return imageName;
 }
 
 @end
